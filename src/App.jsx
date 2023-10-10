8ccada56101df21628ae8cf04aacd8637efaf03e
rume edgar <rumeedgar@icloud.com>
2023-10-10 14:56:52 +0100

    made it possible for user to delete a note

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,19 @@ function App() {
     setNotes(newNotes);
   };
 
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <>
       <div className="container">
-        <NotesList notes={notes} handleAddNote={addNote} />
+        <NotesList
+          notes={notes}
+          handleAddNote={addNote}
+          handleDeleteNote={deleteNote}
+        />
       </div>
     </>
   );
