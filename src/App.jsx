a2c73e8a10ddc0c6930430b0e0cc1840685f69e8
rume edgar <rumeedgar@icloud.com>
2023-10-10 14:35:56 +0100

    adding new notes component

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,22 @@ function App() {
       date: "13/10/2023",
     },
   ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
   return (
     <>
       <div className="container">
-        <NotesList notes={notes} />
+        <NotesList notes={notes} handleAddNote={addNote} />
       </div>
     </>
   );
